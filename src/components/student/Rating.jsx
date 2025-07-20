5aa8a56ee4a421d67abda98801eb31ef16a2925d
Rayyan-khalid <reyyankhalid535@gmail.com>
2025-07-20 05:40:01 +0500

    Add initial React app

---
+++ src/components/student/Rating.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rating = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default Rating
---
+++ src/pages/student/CoursesList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CoursesList = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Course List</h1>
+          <p className='text-gray=500'>
+            <span className='text-blue-600 cursor-pointer'>Home</span> / <span> course List </span>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CoursesList
