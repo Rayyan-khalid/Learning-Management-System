f777b5a3de4343e067e14fefe21f5f9be124911b
Rayyan-khalid <reyyankhalid535@gmail.com>
2025-11-07 22:56:49 +0500

    first commit

---
+++ server/controllers/webhooks.js
@@ -0,0 +1,58 @@
+import pkg from "svix";
+const { Webhook } = pkg;
+
+import User from '../models/User.js'
+
+//API contoller Function to manage Clerk User with databse
+
+export const clerkWebhooks = async (req, res)=>{
+    try {
+        const whook = new webhook(process.env.CLERK_WEBHOOK_SECRET)
+        // This line is the security check.
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-signature"],
+            "svix-signature": req.headers["svix-signature"]
+        })
+
+        const {data, type} = req.body
+
+        switch (type) {
+            case 'user.created': {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url,
+                }
+                await User.create(userData)
+                res.JSON({})
+                
+                break;
+            }
+
+            case 'user.updated' : {
+                const userData = {
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_namw,
+                    imageUrl: data.image_url,
+                }
+                await User.findByIdAndUpdate(data.id,userData)
+                res.json({})
+                break;
+            }
+
+            case 'user.deleted' : {
+                await User.findByIdAndDelete(data.id)
+                res.json({})
+                break;
+            }
+
+            default:
+                break;
+        }
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
