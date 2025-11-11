5b59ffdd7c632ddcf66a6c83455a3dc4a988c947
Rayyan-khalid <reyyankhalid535@gmail.com>
2025-11-11 20:55:53 +0500

    create multer to upload image, create middleware

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { updateRoleToEducator } from '../controllers/educatorController.js'
+import { addCourse, getEducatorCourses, updateRoleToEducator } from '../controllers/educatorController.js'
+import upload from '../configs/multer.js'
+import { ProtectEducator } from '../middlewares/authMiddleware.js'
 
 const educatorRouter = express.Router()
 
 //Add Educator Role
 educatorRouter.get('/update-role', updateRoleToEducator)
+educatorRouter.post('/add-course', upload.single('image'), ProtectEducator, addCourse)
+educatorRouter.get('/courses', ProtectEducator, getEducatorCourses)
 
 export default educatorRouter;
